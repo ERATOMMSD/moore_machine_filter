c949e6107ed1313548264b9917bb13a58fe87de3
Masaki Waga <masakiwaga@gmail.com>
2018-03-26 16:52:14 +0900

    fixed monaa path

--- experiments/run_gtime_maunaloa_slave.sh
+++ experiments/run_gtime_maunaloa_slave.sh
@@ -12,7 +12,7 @@ mkdir -p /home/mwaga/Data/timedPatternMatching
 cd /home/mwaga/Data/timedPatternMatching && wget 'https://drive.google.com/uc?export=download&id=1ZHpRv7_fSmzW879unNlEHjx_wpZRYpiD' -O torque.tar.xz && tar xvf torque.tar.xz
 
 ## Set up latest MONAA
-cd /home/mwaga/monaa && git pull && cd build && make monaa
+cd /home/mwaga && rm -rf monaa && git clone https://github.com/MasWag/monaa.git && cd monaa && mkdir build && cd build && cmake -DCMAKE_BUILD_TYPE=Release .. && make
 
 if (($buf_size != 0)); then
     cd ../build && cmake -DCMAKE_BUILD_TYPE=Release -DBUFFER_SIZE=${buf_size} -DCMAKE_CXX_COMPILER=g++-7 .. && make -j3 filt && cd -
