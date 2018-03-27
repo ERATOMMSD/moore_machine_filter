213a89883fde0c1ffd73b9bb58af9f24b4c53a8e
Masaki Waga <masakiwaga@gmail.com>
2018-03-27 14:06:27 +0000

    works on maunaloa

--- experiments/make_filt-torque-wc.sh
+++ experiments/make_filt-torque-wc.sh
@@ -4,6 +4,6 @@ cd $(dirname $0)
 readonly dir=$(dirname $1)
 
 for file in $dir/$2*.txt; do
-    basename ${file%%.*} | ggrep -o '[0-9]*' | gsed 'N;N;s/\n/ /g' #| awk '{print $3,$1}'
+    basename $file | ggrep -o '[0-9]*' | xargs  
     cat $file | wc -l 
 done | xargs -n 1 | xargs -n 3 | awk '{print $2,$1,$3}' | sort -n -k 1,1 | sort -sn -k 2,2 > $1
--- experiments/make_no-filt-time.sh
+++ experiments/make_no-filt-time.sh
@@ -4,7 +4,7 @@ cd $(dirname $0)
 readonly dir=$(dirname $1)
 
 for file in $dir/$2*.log; do
-    basename ${file%%.*} | grep -o '[0-9]\+' | tail -n 1
+    basename $file | ggrep -o '[0-9]*' | tail -n 2 | head -n 1
     ./extract_execution_time.sh $file
 done | xargs -n 2 | awk '{c[$1]+=1;time[$1]+=$2}END{{for (key in c){print key,time[key]/c[key]}}}' | sort -n > /tmp/no-filt-time.tmp
 for a in {1..18}; do
